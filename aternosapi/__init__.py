d1bce2bc8cf87f906d3f1f14b33c143394727f62
Pritam Das <pritamdas.pd2005@gmail.com>
2021-08-26 17:56:23 +0530

    mcscraper bot

---
+++ aternosapi/__init__.py
@@ -0,0 +1,119 @@
+# import requests
+# from bs4 import BeautifulSoup
+import undetected_chromedriver as uc
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+chrome_options = uc.ChromeOptions()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument('--disable-gpu')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--blink-settings=imagesEnabled=false")
+
+# driver.get("http://aternos.org/s")
+# for cookie in common_cookies:
+#     driver.add_cookie(common_cookies[cookie])
+
+class AternosAPI():
+    def __init__(self, srvcookie, common_cookies):
+        self.driver = uc.Chrome(options=chrome_options)
+        self.driver.get("http://aternos.org/s")
+        self.driver.delete_all_cookies()
+        for cookie in common_cookies:
+            self.driver.add_cookie(common_cookies[cookie])
+        self.driver.add_cookie(srvcookie)
+        self.driver.get("http://aternos.org/server")
+
+    def GetStatus(self):
+        # print("status")
+        status = self.driver.find_element(By.CLASS_NAME, "statuslabel-label").text
+        self.driver.close()
+        return status
+
+    def StartServer(self):
+        # print("start")
+        button = self.driver.find_element(By.ID, "start")
+        if (status := self.driver.find_element(By.CLASS_NAME, "statuslabel-label")).text == "Offline":
+            button.click()
+            f = False
+            while "Online" not in status.text: 
+                # print(status.text)
+                if "Waiting in queue" in status.text and not f:
+                    WebDriverWait(self.driver, 100).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[2]/main/div/div/div/header/span'))).click()
+                    button = WebDriverWait(self.driver, 300).until(EC.element_to_be_clickable((By.ID, 'confirm')))
+                    button.click()
+                    f = True
+                else:
+                    if "Loading" in status.text:
+                        return "server started"
+                    elif "Starting" in status.text:
+                        return "server started"
+                    elif "Preparing" in status.text:
+                        return "server started"
+            self.driver.close()
+            return "server started"
+        else:
+            self.driver.close()
+            return "server is already online"
+
+    def StopServer(self):
+        # print("stop")
+        button = self.driver.find_element(By.ID, "stop")
+        if (status := self.driver.find_element(By.CLASS_NAME, "statuslabel-label")).text == "Online":
+            button.click()
+            while "Offline" not in status.text:
+                if "Saving" in status.text:
+                    return "server stopped"
+                elif "Stopping" in status.text:
+                    return "server stopped"
+            self.driver.close()
+            return "server stopped"
+        else:
+            self.driver.close()
+            return "server is already offline"
+
+    def GetServerInfo(self):
+        # print("st")
+
+        Software = self.driver.find_element(By.ID, "software").text
+
+        Version = self.driver.find_element(By.ID, "version").text
+
+        Status = self.driver.find_element(By.CLASS_NAME, "statuslabel-label").text
+
+        Players = "0/NA"
+
+        if Status == "Online":
+            Players = self.driver.find_element(By.XPATH, "/html/body/div[2]/main/section/div[3]/div[5]/div[2]/div[1]/div[1]/div[2]/div[2]").text
+        self.driver.close()
+
+        return {
+            "software": Software,
+            "version": Version,
+            "status": Status,
+            "players": Players
+        }
+
+        # if(Software in self.JavaSoftwares):
+        #     IP = ServerInfo.find('div', class_='server-ip mobile-full-width').get_text()
+        #     IP = IP.strip()
+        #
+        #     IP = IP.split(" ") /html/body/div[2]/main/section/div[3]/div[5]/div[2]/div[1]/div[1]/div[2]/div[2]
+        #     IP = IP[0].strip()
+        #
+        #     Port = "25565(Optional)"
+        #
+        #     # return f"{IP},{Port},{Software}"
+        #     return Software
+        #
+        # elif(Software in self.BedrockSoftwares):
+        #     IP = ServerInfo.find('span', id='ip').get_text()
+        #     IP = IP.strip()
+        #
+        #     Port = ServerInfo.find('span', id='port').get_text()
+        #     Port = Port.strip()
+        #
+        #     # return f"{IP},{Port},{Software}"
+        #     return Software
---
+++ bot.py
@@ -0,0 +1,89 @@
+# from _typeshed import NoneType
+# import discord
+from discord.ext import commands
+from aternosapi import AternosAPI
+from os import getcwd
+from json import load
+
+with open(getcwd()+"/credentials.json") as creds:
+    credentials = load(creds)
+
+with open(getcwd()+"/servers.json") as srvs:
+    servers = load(srvs)
+
+bot = commands.Bot(command_prefix='$')
+
+@bot.command(name="list-servers", pass_context=True, help="Lists available servers")
+async def list_servers(ctx):
+    resp = []
+    for i, server in enumerate(list(servers.keys())):
+        resp.append(str(i+1)+": "+server)
+    await ctx.send("The registered servers are: \n"+"\n".join(resp))
+
+@bot.command(pass_context=True, help="States the current state of the mentioned server")
+async def status(ctx, srv_no):
+    try:
+        srv_name = list(servers.keys())[int(srv_no)-1]
+        api = AternosAPI(servers[srv_name]["server_cookie"], credentials["common_cookies"])
+        await ctx.send(srv_name+" server is curently "+api.GetStatus())
+        api.driver.quit()
+    except IndexError:
+        await ctx.send("Such a entry do not exist")
+
+@bot.command(pass_context=True, help="Startes the mentioned server")
+async def start(ctx, srv_no):
+    try:
+        srv_name = list(servers.keys())[int(srv_no)-1]
+        api = AternosAPI(servers[srv_name]["server_cookie"], credentials["common_cookies"])
+        sresp = api.StartServer()
+        # print(str(sresp))
+        if sresp != "something went wrong":
+            await ctx.send(srv_name+" "+sresp)
+        else :
+            await ctx.send(sresp)
+        api.driver.quit()
+    except IndexError:
+        await ctx.send("Such a entry do not exist")
+    except TypeError:
+        await ctx.send("something went wrong")
+
+@bot.command(pass_context=True, help="Stopes the mentioned server REQUIRED ROLE: MCS manager")
+@commands.has_role("MCS manager")
+async def stop(ctx, srv_no):
+    try:
+        srv_name = list(servers.keys())[int(srv_no)-1]
+        api = AternosAPI(servers[srv_name]["server_cookie"], credentials["common_cookies"])
+        sresp = api.StopServer()
+        # print(str(sresp))
+        if sresp != "something went wrong":
+            await ctx.send(srv_name+" "+sresp)
+        else :
+            await ctx.send(sresp)
+        api.driver.quit()
+    except IndexError:
+        await ctx.send("Such a entry do not exist")
+    except TypeError:
+        await ctx.send("something went wrong")
+
+@bot.command(pass_context=True, help="States the information about the mentioned server")
+async def getinfo(ctx, srv_no):
+    try:
+        srv_name = list(servers.keys())[int(srv_no)-1]
+        api = AternosAPI(servers[srv_name]["server_cookie"], credentials["common_cookies"])
+        sresp = api.GetServerInfo()
+        await ctx.send(srv_name+"\nIP: "+servers[srv_name]["ip"]+"\nPort: "+servers[srv_name]["port"]+"\nSoftware: "+sresp["software"]+"\nVersion: "+sresp["version"]+"\nPlayers: "+sresp["players"]+"\nStatus: "+sresp["status"])
+        # print(api.GetServerInfo())
+        api.driver.quit()
+    except IndexError:
+        await ctx.send("Such a entry do not exist")
+
+# @bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, (commands.MissingRole, commands.MissingAnyRole)):
+        await ctx.send("You are laking proper roles to run this command")
+    elif isinstance(error, commands.CommandNotFound):
+        await ctx.send("Such a command do not exist")
+    elif isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send("Please mention a server number e.g. 1")
+
+bot.run(credentials["secret_key"])
